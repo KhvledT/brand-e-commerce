66401aedec4e586deb208bdeec5540ae7ade38c2
KhvledT <khaledtareq26@gmail.com>
2025-10-04 22:02:52 +0300

    fix landing page

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Image from "next/image";
 import heroSection1 from "@/assets/imgs/home page imgs/heroSection 01.svg";
-import heroSection2 from "@/assets/imgs/home page imgs/heroSection 02.svg";
+import heroSection2 from "@/assets/imgs/home page imgs/Frame 458.png";
+import heroSection3 from "@/assets/imgs/home page imgs/heroSection 02.svg";
 
 export default function HeroSection() {
   return (
@@ -15,9 +16,18 @@ export default function HeroSection() {
             height={600}
             className="w-full h-auto"
         />
-        <div className="relative w-full h-auto">
+        <div className="relative block md:hidden w-full h-auto">
           <Image
             src={heroSection2}
+            alt="Hero Section 2"
+            width={600}
+            height={600}
+            className="w-full h-auto -translate-y-10"
+          />
+        </div>
+        <div className="relative hidden md:block w-full h-auto">
+          <Image
+            src={heroSection3}
             alt="Hero Section 2"
             width={600}
             height={600}
